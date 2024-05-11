305236906f18044eeeae05a875fed655a8a43318
NouraChan <162762157+NouraChan@users.noreply.github.com>
2024-05-11 13:03:36 +0300

    get api for posts

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/category', [CategoryController::class ,'all']);
+Route::get('/post', [PostController::class ,'all']);
